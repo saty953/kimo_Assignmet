03cd1a5ed6dc4733063c409b3cae4775bb35a8ed
Satyam Singh Chauhan <107349203+saty953@users.noreply.github.com>
2023-04-08 13:01:42 +0530

    second commit welcome page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
+import FirstPage from "./Components/FirstPage";
+import SecondPage from "./Components/SecondPage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        hko
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<FirstPage />} />
+        </Routes>
+        <Routes>
+          <Route path="/second" element={<SecondPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
